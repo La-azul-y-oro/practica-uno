2119413a2bb982da0bf9842e21a31781fe5f252a
Gabriel Pereyra <69758300+gabopereyra@users.noreply.github.com>
2024-05-11 15:58:03 -0300

    add @CrossOrigin annotation to enable API consumes. Fix import scripts

--- src/main/java/com/taller/practica/controllers/UsuarioController.java
+++ src/main/java/com/taller/practica/controllers/UsuarioController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
+@CrossOrigin
 public class UsuarioController {
     @Autowired
     private UsuarioRepository usuarioRepository;
