5d22e79e413db3cf5b1bab08184b0170369122ae
Gabriel Pereyra <gabrielp1689@gmail.com>
2024-05-11 15:53:18 -0300

    add @CrossOrigin annotation to enable API consumes. Fix import scripta

--- src/main/java/com/taller/practica/entities/Usuario.java
+++ src/main/java/com/taller/practica/entities/Usuario.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Data
 public class Usuario {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(nullable = false)
     private String username;
