6fe4596b670ce9f505219fe01575c5e3577cca79
Gabriel Pereyra <69758300+gabopereyra@users.noreply.github.com>
2024-05-18 18:13:54 -0300

    1505
    * add @CrossOrigin annotation to enable API consumes. Fix import scripta
    
    * change generation strategy. change data type to boolean native in Usuario and put it in true by default

--- src/main/java/com/taller/practica/entities/Persona.java
+++ src/main/java/com/taller/practica/entities/Persona.java
@@ -11,7 +11,7 @@ import java.util.Set;
 @Setter
 public class Persona {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
     @ManyToMany(mappedBy = "personas")
